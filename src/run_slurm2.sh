cb786c6b2c2bf12fd1f5584dade60dd8c575656a
Johannes Getzner <j.getzner@gmail.com>
2023-07-16 17:54:22 +0200

    second slurm run file

---
+++ src/run_slurm2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --partition=master
+#SBATCH --ntasks=1
+#SBATCH --time=07-00:00:00
+#SBATCH --mem=32G
+#SBATCH --gres=gpu:1
+
+# Load the Anaconda module
+module load python/anaconda3
+
+# Specify the Anaconda environment name
+ANACONDA_ENV="fix"
+
+# Specify the full path to the Anaconda environment's Python interpreter
+ANACONDA_PYTHON="/u/home/getzner/.conda/envs/$ANACONDA_ENV/bin/python"
+
+# Activate the Anaconda environment
+source activate $ANACONDA_ENV
+
+# Your commands using the Anaconda environment
+echo "Running script with Anaconda environment: $ANACONDA_ENV"
+$ANACONDA_PYTHON train.py --run_name dp_2 --reverse
