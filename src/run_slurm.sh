288190554c3c3c205dddf9ede0925a1c6dee2411
JohannesGetzner <j.getzner@gmail.com>
2023-08-01 22:08:38 +0200

    changed slurm configs

--- src/run_slurm.sh
+++ src/run_slurm.sh
@@ -20,8 +20,8 @@ source activate $ANACONDA_ENV
 
 # Your commands using the Anaconda environment
 echo "Running script with Anaconda environment: $ANACONDA_ENV"
-$ANACONDA_PYTHON train.py --run_name dp_1 --protected_attr_percent 0.0 --d "${date}"
+#$ANACONDA_PYTHON train.py --run_name dp_1 --protected_attr_percent 0.0 --d "${date}"
 $ANACONDA_PYTHON train.py --run_name dp_1 --protected_attr_percent 0.25 --d "${date}"
 $ANACONDA_PYTHON train.py --run_name dp_1 --protected_attr_percent 0.50 --d "${date}"
 $ANACONDA_PYTHON train.py --run_name dp_1 --protected_attr_percent 0.75 --d "${date}"
-$ANACONDA_PYTHON train.py --run_name dp_1 --protected_attr_percent 1.0 --d "${date}"
+#$ANACONDA_PYTHON train.py --run_name dp_1 --protected_attr_percent 1.0 --d "${date}"
