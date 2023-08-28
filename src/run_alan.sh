b0466d342aec2bcbbe27c133da21e295ff650826
Johannes Getzner <j.getzner@gmail.com>
2023-08-28 13:28:27 +0200

    changed config for alan runs

--- src/run_alan.sh
+++ src/run_alan.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 date=$(date '+%Y-%m-%d %H:%M:%S')
-python train.py --run_config normal --run_version v1 --protected_attr_percent 0.75 --second_stage_epsilon -1 --d "${date}" --group_name_mod "bs32-ss"
-python train.py --run_config normal --run_version v1 --protected_attr_percent 1.00 --second_stage_epsilon -1 --d "${date}" --group_name_mod "bs32-ss"
-date=$(date '+%Y-%m-%d %H:%M:%S')
-python train.py --run_config normal --run_version v3 --protected_attr_percent 0.00 --d "${date}" --group_name_mod "bs64"
-python train.py --run_config normal --run_version v3 --protected_attr_percent 0.25 --d "${date}" --group_name_mod "bs64"
-python train.py --run_config normal --run_version v3 --protected_attr_percent 0.50 --d "${date}" --group_name_mod "bs64"
-python train.py --run_config normal --run_version v3 --protected_attr_percent 0.75 --d "${date}" --group_name_mod "bs64"
-python train.py --run_config normal --run_version v3 --protected_attr_percent 1.00 --d "${date}" --group_name_mod "bs64"
+python train.py --run_config normal --run_version v1 --protected_attr_percent 0.00 --second_stage_epsilon -1 --second_stage_epochs 130 --d "${date}" --group_name_mod "bs32-ss"
+python train.py --run_config normal --run_version v1 --protected_attr_percent 0.25 --second_stage_epsilon -1 --second_stage_epochs 130 --d "${date}" --group_name_mod "bs32-ss"
+python train.py --run_config normal --run_version v1 --protected_attr_percent 0.50 --second_stage_epsilon -1 --second_stage_epochs 130 --d "${date}" --group_name_mod "bs32-ss"
+python train.py --run_config normal --run_version v1 --protected_attr_percent 0.75 --second_stage_epsilon -1 --second_stage_epochs 130 --d "${date}" --group_name_mod "bs32-ss"
+python train.py --run_config normal --run_version v1 --protected_attr_percent 1.00 --second_stage_epsilon -1 --second_stage_epochs 130 --d "${date}" --group_name_mod "bs32-ss"
