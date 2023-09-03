113069d0e62ad2aba6b39ae3b4749c8c5ec97749
JohannesGetzner <j.getzner@gmail.com>
2023-09-03 14:51:36 +0200

    small experiment where old data is just dropped

--- src/run_slurm_2.sh
+++ src/run_slurm_2.sh
@@ -22,17 +22,9 @@ source activate $ANACONDA_ENV
 # Your commands using the Anaconda environment
 echo "Running script with Anaconda environment: $ANACONDA_ENV"
 
-$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.55 --d "${date}" --group_name_mod "bs32"
-$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.60 --d "${date}" --group_name_mod "bs32"
-$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.65 --d "${date}" --group_name_mod "bs32"
-$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.70 --d "${date}" --group_name_mod "bs32"
-$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.75 --d "${date}" --group_name_mod "bs32"
-$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.80 --d "${date}" --group_name_mod "bs32"
-$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.85 --d "${date}" --group_name_mod "bs32"
-$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.90 --d "${date}" --group_name_mod "bs32"
-$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.95 --d "${date}" --group_name_mod "bs32"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.0 --d "${date}" --group_name_mod "bs32_dropold"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.25 --d "${date}" --group_name_mod "bs32_dropold"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.50 --d "${date}" --group_name_mod "bs32_dropold"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.75 --d "${date}" --group_name_mod "bs32_dropold"
 
 #$ANACONDA_PYTHON -u train.py --run_config dp --run_version v1 --protected_attr_percent 0.80 --upsampling_strategy "even" --d "${date}" --group_name_mod "bs512-mgn001-upsamplingeven"
-#$ANACONDA_PYTHON -u train.py --run_config dp --run_version v1 --protected_attr_percent 0.85 --upsampling_strategy "even" --d "${date}" --group_name_mod "bs512-mgn001-upsamplingeven"
-#$ANACONDA_PYTHON -u train.py --run_config dp --run_version v1 --protected_attr_percent 0.90 --upsampling_strategy "even" --d "${date}" --group_name_mod "bs512-mgn001-upsamplingeven"
-#$ANACONDA_PYTHON -u train.py --run_config dp --run_version v1 --protected_attr_percent 0.95 --upsampling_strategy "even" --d "${date}" --group_name_mod "bs512-mgn001-upsamplingeven"
