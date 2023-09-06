70b81eeabd1b869297d2c0bb44476a30688bb1fa
JohannesGetzner <j.getzner@gmail.com>
2023-09-06 13:57:33 +0200

    added line to assign deprecated tag to runs

--- edit_wandb_runs.py
+++ edit_wandb_runs.py
@@ -1,11 +1,12 @@
 import wandb
 api = wandb.Api()
-current = "2023.08.19-15:49:47-FAE-rsna-age-SWEEP-lr-bs1024-mgn001-old-down-weighted-DP"
-target = "2023-08-19 15:49:47-FAE-rsna-age-SWEEP-lr-bs1024-mgn001-old-down-weighted-DP"
+current = "2023-09-01 14:30:01-FAE-rsna-age-bs512-mgn001-upsamplingeven-DP-DEPRECATED"
+target = "2023-09-01 14:30:01-FAE-rsna-age-bs512-mgn001-upsamplingeven-DP-DEPRECATED"
 for r in api.runs(
         "thesis-getzner/unsupervised-fairness",
         filters={
             "group": current
         }):
     r.group = target
+    # r.tags = r.tags + ["DEPRECATED"]
     r.update()
