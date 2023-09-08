2bc50a3525b58a76f4408f2343e8056cc0184d15
JohannesGetzner <j.getzner@gmail.com>
2023-09-08 11:43:48 +0200

    added new configuration and changed wrong group name mod

--- src/run_slurm_2.sh
+++ src/run_slurm_2.sh
@@ -26,10 +26,10 @@ $ANACONDA_PYTHON -u train.py --run_config dp --run_version v1 --protected_attr_p
 $ANACONDA_PYTHON -u train.py --run_config dp --run_version v1 --protected_attr_percent 0.70 --upsampling_strategy "even" --custom_sr --d "${date}" --group_name_mod "bs512-mgn001-upsamplingeven-csr"
 $ANACONDA_PYTHON -u train.py --run_config dp --run_version v1 --protected_attr_percent 0.80 --upsampling_strategy "even" --custom_sr --d "${date}" --group_name_mod "bs512-mgn001-upsamplingeven-csr"
 date=$(date '+%Y-%m-%d %H:%M:%S')
-$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.60 --d "${date}" --group_name_mod "bs512-mgn001"
-$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.70 --d "${date}" --group_name_mod "bs512-mgn001"
-$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.80 --d "${date}" --group_name_mod "bs512-mgn001"
-$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.90 --d "${date}" --group_name_mod "bs512-mgn001"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.60 --d "${date}" --group_name_mod "bs32"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.70 --d "${date}" --group_name_mod "bs32"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.80 --d "${date}" --group_name_mod "bs32"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.90 --d "${date}" --group_name_mod "bs32"
 date=$(date '+%Y-%m-%d %H:%M:%S')
 $ANACONDA_PYTHON -u train.py --run_config dp --run_version v1 --protected_attr_percent 0.60 --upsampling_strategy "even" --d "${date}" --group_name_mod "bs512-mgn001-upsamplingeven"
 $ANACONDA_PYTHON -u train.py --run_config dp --run_version v1 --protected_attr_percent 0.70 --upsampling_strategy "even" --d "${date}" --group_name_mod "bs512-mgn001-upsamplingeven"
