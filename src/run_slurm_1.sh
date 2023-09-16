7cb10993cba9fbbee10680ec0111b41d613f4980
JohannesGetzner <j.getzner@gmail.com>
2023-09-16 11:09:55 +0200

    fixed configuration

--- src/run_slurm_1.sh
+++ src/run_slurm_1.sh
@@ -5,8 +5,6 @@
 #SBATCH --mem=32G
 #SBATCH --gres=gpu:1
 
-date=$(date '+%Y-%m-%d %H:%M:%S')
-
 # Load the Anaconda module
 module load python/anaconda3
 
@@ -22,27 +20,44 @@ source activate $ANACONDA_ENV
 # Your commands using the Anaconda environment
 echo "Running script with Anaconda environment: $ANACONDA_ENV"
 
-$ANACONDA_PYTHON -u train.py --run_config normal --run_version v2 --protected_attr_percent 0.01 --upsampling_strategy "even-male" --d "${date}" --group_name_mod "bs32-upsamplingevenmale"
+date=$(date '+%Y-%m-%d %H:%M:%S')
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v2 --protected_attr_percent 0.00 --upsampling_strategy "even-male" --d "${date}" --group_name_mod "bs32-upsamplingevenmale"
 $ANACONDA_PYTHON -u train.py --run_config normal --run_version v2 --protected_attr_percent 0.10 --upsampling_strategy "even-male" --d "${date}" --group_name_mod "bs32-upsamplingevenmale"
 $ANACONDA_PYTHON -u train.py --run_config normal --run_version v2 --protected_attr_percent 0.20 --upsampling_strategy "even-male" --d "${date}" --group_name_mod "bs32-upsamplingevenmale"
 $ANACONDA_PYTHON -u train.py --run_config normal --run_version v2 --protected_attr_percent 0.30 --upsampling_strategy "even-male" --d "${date}" --group_name_mod "bs32-upsamplingevenmale"
 $ANACONDA_PYTHON -u train.py --run_config normal --run_version v2 --protected_attr_percent 0.40 --upsampling_strategy "even-male" --d "${date}" --group_name_mod "bs32-upsamplingevenmale"
-date=$(date '+%Y-%m-%d %H:%M:%S')
-$ANACONDA_PYTHON -u train.py --run_config normal --run_version v2 --protected_attr_percent 0.01 --d "${date}" --group_name_mod "bs32"
-$ANACONDA_PYTHON -u train.py --run_config normal --run_version v2 --protected_attr_percent 0.10 --d "${date}" --group_name_mod "bs32"
-$ANACONDA_PYTHON -u train.py --run_config normal --run_version v2 --protected_attr_percent 0.20 --d "${date}" --group_name_mod "bs32"
-$ANACONDA_PYTHON -u train.py --run_config normal --run_version v2 --protected_attr_percent 0.30 --d "${date}" --group_name_mod "bs32"
-$ANACONDA_PYTHON -u train.py --run_config normal --run_version v2 --protected_attr_percent 0.40 --d "${date}" --group_name_mod "bs32"
 
 date=$(date '+%Y-%m-%d %H:%M:%S')
-$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.01 --upsampling_strategy "even-old" --d "${date}" --group_name_mod "bs32-upsamplingevenold"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.00 --upsampling_strategy "even-old" --d "${date}" --group_name_mod "bs32-upsamplingevenold"
 $ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.10 --upsampling_strategy "even-old" --d "${date}" --group_name_mod "bs32-upsamplingevenold"
 $ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.20 --upsampling_strategy "even-old" --d "${date}" --group_name_mod "bs32-upsamplingevenold"
 $ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.30 --upsampling_strategy "even-old" --d "${date}" --group_name_mod "bs32-upsamplingevenold"
 $ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.40 --upsampling_strategy "even-old" --d "${date}" --group_name_mod "bs32-upsamplingevenold"
+
+
 date=$(date '+%Y-%m-%d %H:%M:%S')
-$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.01 --d "${date}" --group_name_mod "bs32"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.00 --d "${date}" --group_name_mod "bs32"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.05 --d "${date}" --group_name_mod "bs32"
 $ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.10 --d "${date}" --group_name_mod "bs32"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.15 --d "${date}" --group_name_mod "bs32"
 $ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.20 --d "${date}" --group_name_mod "bs32"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.25 --d "${date}" --group_name_mod "bs32"
 $ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.30 --d "${date}" --group_name_mod "bs32"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.35 --d "${date}" --group_name_mod "bs32"
 $ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.40 --d "${date}" --group_name_mod "bs32"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.45 --d "${date}" --group_name_mod "bs32"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.50 --d "${date}" --group_name_mod "bs32"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 1.00 --d "${date}" --group_name_mod "bs32"
+date=$(date '+%Y-%m-%d %H:%M:%S')
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v2 --protected_attr_percent 0.00 --d "${date}" --group_name_mod "bs32"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v2 --protected_attr_percent 0.05 --d "${date}" --group_name_mod "bs32"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v2 --protected_attr_percent 0.10 --d "${date}" --group_name_mod "bs32"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v2 --protected_attr_percent 0.15 --d "${date}" --group_name_mod "bs32"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v2 --protected_attr_percent 0.20 --d "${date}" --group_name_mod "bs32"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v2 --protected_attr_percent 0.25 --d "${date}" --group_name_mod "bs32"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v2 --protected_attr_percent 0.30 --d "${date}" --group_name_mod "bs32"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v2 --protected_attr_percent 0.35 --d "${date}" --group_name_mod "bs32"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v2 --protected_attr_percent 0.40 --d "${date}" --group_name_mod "bs32"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v2 --protected_attr_percent 0.45 --d "${date}" --group_name_mod "bs32"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v2 --protected_attr_percent 0.50 --d "${date}" --group_name_mod "bs32"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v2 --protected_attr_percent 1.00 --d "${date}" --group_name_mod "bs32"
