16a4f6d880d8659900ffdafb6f9f2cb08a934b34
JohannesGetzner <j.getzner@gmail.com>
2023-09-17 20:05:40 +0200

    added more model-size experiments, reducing the number of blocks

---
+++ src/run_slurm_7.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --partition=master
+#SBATCH --ntasks=1
+#SBATCH --time=07-00:00:00
+#SBATCH --mem=32G
+#SBATCH --gres=gpu:1
+
+# Load the Anaconda module
+module load python/anaconda3
+
+# Specify the Anaconda environment name
+ANACONDA_ENV="thesis_opacus"
+
+# Specify the full path to the Anaconda environment's Python interpreter
+ANACONDA_PYTHON="/u/home/getzner/.conda/envs/$ANACONDA_ENV/bin/python"
+
+# Activate the Anaconda environment
+source activate $ANACONDA_ENV
+
+# Your commands using the Anaconda environment
+echo "Running script with Anaconda environment: $ANACONDA_ENV"
+
+date=$(date '+%Y-%m-%d %H:%M:%S')
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.5 --hidden_dims 100 150 200  --d "${date}" --group_name_mod "bs32-ms" --job_type_mod "ms100-150-200"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.5 --hidden_dims 100 150 150  --d "${date}" --group_name_mod "bs32-ms" --job_type_mod "ms100-150-150"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.5 --hidden_dims 100 150      --d "${date}" --group_name_mod "bs32-ms" --job_type_mod "ms100-150"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.5 --hidden_dims 100 150      --d "${date}" --group_name_mod "bs32-ms" --job_type_mod "ms100-150"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.5 --hidden_dims 100          --d "${date}" --group_name_mod "bs32-ms" --job_type_mod "ms100"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.5 --hidden_dims 50           --d "${date}" --group_name_mod "bs32-ms" --job_type_mod "ms50"
