dc9d28a84716a49c8e3a88a584aff320305827ba
JohannesGetzner <j.getzner@gmail.com>
2023-09-17 14:46:47 +0200

    modified model size experiment run configs

--- src/run_slurm_2.sh
+++ src/run_slurm_2.sh
@@ -21,9 +21,17 @@ source activate $ANACONDA_ENV
 echo "Running script with Anaconda environment: $ANACONDA_ENV"
 
 date=$(date '+%Y-%m-%d %H:%M:%S')
-$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.25 --hidden_dims 1 1 1 1  --d "${date}" --group_name_mod "bs32-ms" --job_type_mod "ms1-1-1-1"
-$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.25 --hidden_dims 1 1 2 2  --d "${date}" --group_name_mod "bs32-ms" --job_type_mod "ms1-1-2-2"
-#$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.25 --hidden_dims 1 2 3 4  --d "${date}" --group_name_mod "bs32-ms" --job_type_mod "ms1-2-3-4"
-#$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.25 --hidden_dims 1 3 5 7  --d "${date}" --group_name_mod "bs32-ms" --job_type_mod "ms1-3-5-7"
-#$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.25 --hidden_dims 1 4 7 10 --d "${date}" --group_name_mod "bs32-ms" --job_type_mod "ms1-4-7-10"
-#$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.25 --hidden_dims 2 5 8 11 --d "${date}" --group_name_mod "bs32-ms" --job_type_mod "ms2-5-8-11"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.5 --hidden_dims 1 1 1 1  --d "${date}" --group_name_mod "bs32-ms" --job_type_mod "ms1-1-1-1"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.5 --hidden_dims 1 1 2 2  --d "${date}" --group_name_mod "bs32-ms" --job_type_mod "ms1-1-2-2"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.75 --hidden_dims 1 1 1 1  --d "${date}" --group_name_mod "bs32-ms" --job_type_mod "ms1-1-1-1"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.75 --hidden_dims 1 1 2 2  --d "${date}" --group_name_mod "bs32-ms" --job_type_mod "ms1-1-2-2"
+
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.5 --hidden_dims 1 2 3 4  --d "${date}" --group_name_mod "bs32-ms" --job_type_mod "ms1-2-3-4"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.5 --hidden_dims 1 3 5 7  --d "${date}" --group_name_mod "bs32-ms" --job_type_mod "ms1-3-5-7"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.5 --hidden_dims 1 4 7 10 --d "${date}" --group_name_mod "bs32-ms" --job_type_mod "ms1-4-7-10"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.5 --hidden_dims 2 5 8 11 --d "${date}" --group_name_mod "bs32-ms" --job_type_mod "ms2-5-8-11"
+
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.75 --hidden_dims 1 2 3 4  --d "${date}" --group_name_mod "bs32-ms" --job_type_mod "ms1-2-3-4"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.75 --hidden_dims 1 3 5 7  --d "${date}" --group_name_mod "bs32-ms" --job_type_mod "ms1-3-5-7"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.75 --hidden_dims 1 4 7 10 --d "${date}" --group_name_mod "bs32-ms" --job_type_mod "ms1-4-7-10"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.75 --hidden_dims 2 5 8 11 --d "${date}" --group_name_mod "bs32-ms" --job_type_mod "ms2-5-8-11"
