8b5370041240f6bf0aab80f475ecdd510ce12332
JohannesGetzner <j.getzner@gmail.com>
2023-10-07 21:29:43 +0200

    changed configs

--- src/run_slurm_2.sh
+++ src/run_slurm_2.sh
@@ -22,6 +22,10 @@ echo "Running script with Anaconda environment: $ANACONDA_ENV"
 
 date=$(date '+%Y-%m-%d %H:%M:%S')
 # default --hidden_dims 100 150 200 300
-$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.5 --hidden_dims 100 150 200 250 300                          --group_name_mod "bs32-ms" --job_type_mod "ms100-150-200-250-300"             --d "${date}"
-$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.5 --hidden_dims 100 175 250 325 400                          --group_name_mod "bs32-ms" --job_type_mod "ms100-175-250-325-400"             --d "${date}"
-$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.5 --hidden_dims 100 200 300 400 500                          --group_name_mod "bs32-ms" --job_type_mod "ms100-200-300-400-500"            --d "${date}"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.75 --hidden_dims 100 150 200 250 300  --group_name_mod "bs32-ms" --job_type_mod "ms100-150-200-250-300"  --d "${date}"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.75 --hidden_dims 100 175 250 325 400  --group_name_mod "bs32-ms" --job_type_mod "ms100-175-250-325-400"  --d "${date}"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.75 --hidden_dims 100 200 300 400 500  --group_name_mod "bs32-ms" --job_type_mod "ms100-200-300-400-500" --d "${date}"
+
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.25 --hidden_dims 100 150 200 250 300  --group_name_mod "bs32-ms" --job_type_mod "ms100-150-200-250-300"  --d "${date}"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.25 --hidden_dims 100 175 250 325 400  --group_name_mod "bs32-ms" --job_type_mod "ms100-175-250-325-400"  --d "${date}"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1 --protected_attr_percent 0.25 --hidden_dims 100 200 300 400 500  --group_name_mod "bs32-ms" --job_type_mod "ms100-200-300-400-500" --d "${date}"
