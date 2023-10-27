868e76be212fcae2baedefe4e85a0a4ac3c7db1e
JohannesGetzner <j.getzner@gmail.com>
2023-10-27 21:21:44 +0200

    added new notebook for fea distillation results with RD model

--- edit_wandb_runs.py
+++ edit_wandb_runs.py
@@ -1,7 +1,7 @@
 import wandb
 api = wandb.Api()
-current = ""
-target =  ""
+current = "2023-10-26 20:24:31-FAE-rsna-age-bs32-ms-noDP"
+target =  "2023-10-25 21:54:19-FAE-rsna-age-bs32-ms-noDP"
 for r in api.runs(
         "thesis-getzner/unsupervised-fairness",
         filters={
