6bd5a8f53841411b17e15bae95d54ee713d5aa1b
JohannesGetzner <j.getzner@gmail.com>
2023-10-27 21:25:02 +0200

    modified run script with additional RD experiments on rsna

--- src/run_slurm2.sh
+++ src/run_slurm2.sh
@@ -21,4 +21,6 @@ source activate $ANACONDA_ENV
 echo "Running script with Anaconda environment: $ANACONDA_ENV"
 
 date=$(date '+%Y-%m-%d %H:%M:%S')
-$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1_dataset_distill --protected_attr_percent 0.5 --no_img_log --n_training_samples 1 --model_type "RD" --group_name_mod "bs32-dataset-distillation-nsamples1" --d "${date}"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1_dataset_distill --protected_attr_percent 0.5 --no_img_log --n_training_samples 5 --model_type "RD" --group_name_mod "bs32-dataset-distillation-nsamples5" --d "${date}"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1_dataset_distill --protected_attr_percent 0.5 --no_img_log --n_training_samples 10 --model_type "RD" --group_name_mod "bs32-dataset-distillation-nsamples10" --d "${date}"
+$ANACONDA_PYTHON -u train.py --run_config normal --run_version v1_dataset_distill --protected_attr_percent 0.5 --no_img_log --n_training_samples 15 --model_type "RD" --group_name_mod "bs32-dataset-distillation-nsamples15" --d "${date}"
