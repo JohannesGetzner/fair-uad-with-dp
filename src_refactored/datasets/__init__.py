39eba1b64cdc0e382f9b48f85a2859ef86ed5ea8
JohannesGetzner <j.getzner@gmail.com>
2023-10-31 17:02:47 +0100

    removed src_refactored to new branch

--- src_refactored/datasets/__init__.py
+++
@@ -1,5 +0,0 @@
-import os
-RSNA_DIR = os.environ.get('RSNA_DIR', './datasets/RSNA')
-CXR14_DIR = os.environ.get('CXR14_DIR', 'datasets/CXR8')
-MIMIC_CXR_DIR = os.environ.get('MIMIC-CXR_DIR', 'datasets/MIMIC-CXR/mimic-cxr-jpg_2-0-0')
-CHEXPERT_DIR = os.environ.get('CHEXPERT_DIR', 'datasets/CheXpert')
